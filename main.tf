e9d8250823cc5316e656958f4100e68560f25e72
Kyle Smith <kyle.r.smith@accenture.com>
2021-03-03 13:44:21 -0500

    Common#512 disable auto_minor_version_upgrade on rds instance

--- main.tf
+++ main.tf
@@ -215,7 +215,7 @@ resource "random_integer" "start_minute" {
 resource "aws_db_instance" "database" {
   allocated_storage          = var.allocated_storage
   apply_immediately          = false
-  auto_minor_version_upgrade = true
+  auto_minor_version_upgrade = false
   backup_retention_period    = 14
   backup_window              = local.backup_window
   db_subnet_group_name       = aws_db_subnet_group.subnet_group.name
